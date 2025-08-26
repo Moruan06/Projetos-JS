63295108ef039670e95efa1da54e44fe3789ebe7
Moruan06 <moruan.mattar@gmail.com>
2025-08-25 21:51:34 -0300

    Remove unused getContrastColor function to streamline color change functionality

--- script.js
+++ script.js
@@ -57,20 +57,6 @@ projects.forEach((project) => {
   main.innerHTML += cardHTML;
 });
 
-function getContrastColor(hexColor) {
-  if (hexColor.startsWith("#")) {
-    hexColor = hexColor.slice(1);
-  }
-
-  const r = parseInt(hexColor.substring(0, 2), 16);
-  const g = parseInt(hexColor.substring(2, 4), 16);
-  const b = parseInt(hexColor.substring(4, 6), 16);
-
-  const luminance = (0.299 * r + 0.587 * g + 0.114 * b) / 255;
-
-  return luminance > 0.5 ? "#000000" : "#FFFFFF";
-}
-
 const changeColor = (() => {
   const colorPicker = document.querySelector("#colorPicker");
   
